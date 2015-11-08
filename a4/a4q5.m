27faaba16ce9e1cb61135d349238a1fa0061741b
dougshidong <doug.shidong@gmail.com>
2015-11-08 18:36:17 -0500

    split matlab code

--- a4/a4q5.m
+++ a4/a4q5.m
@@ -1,46 +1,27 @@
-clear;
+clear;clc;
+% QR Factorization Method
+% 1  -  MATLAB Built-in
+% 2  -  Classical Gram-Schmidt
+% 3  -  Modified Gram-Schmidt
 method = 0;
 % Matrix Sizes
-m = 10;
-n = 5;
+m = 30;
+n = 20;
 
-%% Matrix Initialization
-A = zeros(m,n);
-for i = 1 : m
-    for j = 1 : n
-        A(i,j) = (j/n)^(i-1);
-    end
-end
-
+%% Vandermonde Initialization
+A = vanderM(m,n);
 if method == 0
     %% MATLAB QR
     [Q,R] = qr(A,0);
 elseif method == 1
     %% CGS
-    for k = 1 : n
-        for i = 1 : k - 1
-            R(i,k) = Q(:,i)'*A(:,k);
-        end
-        for i = 1 : k - 1
-            A(:,k) = A(:,k) - R(i,k)*Q(:,i);
-        end
-        R(k,k) = norm(A(:,k),2);
-        Q(:,k) = A(:,k)/R(k,k);
-    end
+    [Q,R] = cgs(A);
 elseif method == 2
     %% MGS
-    for k = 1 : n
-        for i = 1 : k - 1
-            R(i,k) = Q(:,i)'*A(:,k);
-            A(:,k) = A(:,k) - R(i,k)*Q(:,i);
-        end
-        R(k,k) = norm(A(:,k),2);
-        Q(:,k) = A(:,k)/R(k,k);
-    end
+    [Q,R] = mgs(A);
 end
+
 %% Output
-Q
-R
 norm(A - Q*R,2)/norm(A,2)
 norm(Q'*Q - eye(n), 2)
 
