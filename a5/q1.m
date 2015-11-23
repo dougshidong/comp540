2f5f8c1a9248586ccbeda0909f283c2b3439736e
dougshidong <doug.shidong@gmail.com>
2015-11-22 21:12:28 -0500

    added comments to q1

--- a5/q1.m
+++ a5/q1.m
@@ -1,7 +1,8 @@
-%%
+%% a1(t) = 1 and a2(t) = t
 clc;clear;
 n = 9;
 
+% Initialization
 t = 1.11 * ones(n,1);
 b = 2.11 * ones(n,1);
 for i = 1 : n
@@ -13,6 +14,7 @@ b(1:3) = b(1:3) + 0.0001;
 
 A = [ones(n,1), t]
 
+% Single Precision QR
 A = single(A);          
 b = single(b);
 
@@ -22,19 +24,21 @@ xqr = A\b
 
 res_qr = norm(b - A*xqr)
 
+% Single Precision Normal Equations
 kaat = cond(A'*A)
 
 xne = (A'*A)\(A'*b)
 
 res_ne = norm(b - A*xne)
 
+% Double Precision QR
 A = double(A);
 b = double(b);
 kab = cond(A)
 xdb = A\b
 res_db = norm(b - A*xdb)
 
-%%
+%% Repeat with a1(t) = 1, a2(t) = (t-1.11)*100
 clear;clc;
 n = 9;
 
@@ -49,6 +53,7 @@ b(1:3) = b(1:3) + 0.0001;
 
 A = [ones(n,1), t];
 
+% Single Precision QR
 A = single(A)
 b = single(b)
 
@@ -58,12 +63,14 @@ xqr = A\b
 
 res_qr = norm(b - A*xqr)
 
+% Single Precision Normal Equations
 kaat = cond(A'*A)
 
 xne = (A'*A)\(A'*b)
 
 res_ne = norm(b - A*xne)
 
+% Double Precision QR
 A = double(A);
 b = double(b);
 kab = cond(A)
