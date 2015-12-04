7a7c624eeacf121fe0b9e0de3823589818646d5a
dougshidong <doug.shidong@gmail.com>
2015-12-03 21:19:38 -0500

    finished q3

---
+++ a6/q3.m
@@ -0,0 +1,48 @@
+%% Initialize
+clear;clc;
+icase = 5;
+switch icase
+    case 1
+        A = [5 4 1 1;
+             4 5 1 1;
+             1 1 4 2;
+             1 1 2 4];
+    case 2
+        A = [6 4 4 1;
+             4 6 1 4;
+             4 1 6 4;
+             1 4 4 6];
+    case 3
+        A = [6 -3 4 1;
+             4  2 4 0;
+             4 -2 3 1;
+             4  2 3 1];
+    case 4
+        A = [4 -5  0  3;
+             0  4 -3 -5;
+             5 -3  4  0;
+             3  0  5  4];
+    case 5
+        A = [10 -19 17 -12 4 1;
+              9 -18 17 -12 4 1;
+              8 -16 15 -11 4 1;
+              6 -12 12 -10 4 1;
+              4  -8  8  -6 1 2;
+              2  -4  4  -3 1 0];
+end
+n = size(A,1);
+
+%% Iterate
+maxIt = 100000;
+tol = 1e-10;
+Ak = A
+for i = 1:maxIt
+    [Qk, Rk] = qr(Ak);
+    Ak = Rk*Qk;
+    lowTsum = sum(sum(abs(tril(Ak,-1))));
+    if lowTsum < tol;
+        break
+    end
+end
+
+Ak
